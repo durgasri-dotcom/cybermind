466fc1ff3a7bf1b05ac345e087c94f61ae7ff2a4
Durga <durgasri720@gmail.com>
2026-03-27 21:50:27 -0400

    fix(lint): fix import style in alembic cves migration file

--- src/backend/database/migrations/versions/568382a274b3_add_cves_table.py
+++ src/backend/database/migrations/versions/568382a274b3_add_cves_table.py
@@ -5,17 +5,16 @@ Revises: 63c50d5f395a
 Create Date: 2026-03-27 21:35:10.011865
 
 """
-from typing import Sequence, Union
+from collections.abc import Sequence
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '568382a274b3'
-down_revision: Union[str, None] = '63c50d5f395a'
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | None = '63c50d5f395a'
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
